1772435d47aeca3b8218f0b40b8660c4ffa35d59
Christopher J Siverio <christopherfacyt@gmail.com>
2020-07-01 20:49:21 -0600

    mejoras

--- routers/router_adn.js
+++ routers/router_adn.js
@@ -4,10 +4,8 @@ import ControllerAdn from '../controllers/controller_adn'
 
 const router = new Router()
 
-// Crear un nuevo ejercicio
 router.post('/mutation', ControllerAdn.detectarMutacion)
 
-// obtener nota por id
 router.get('/stats', ControllerAdn.estadisticas)
 
 export default router
